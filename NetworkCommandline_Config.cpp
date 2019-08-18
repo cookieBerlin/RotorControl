001ee28d2d786dec5e0f9ff5a56e5e66d76bbd69
Angelo Cuccato <Angelo@Cuccato.net>
2019-08-18 12:02:16 +0200

    extend NetworkCommandline_Main with all EEPROM values

--- NetworkCommandline_Config.cpp
+++ NetworkCommandline_Config.cpp
@@ -17,6 +17,13 @@ void Console_Menu_ConfigRead( void);
 void Console_Menu_ConfigWrite( void);
 void Console_Menu_ConfigShow( void);
 void Console_Menu_Config_Help( void);
+void Console_Menu_Config_UseDhcp( void);
+void Console_Menu_Config_SetEncoderCounterScalerDown( void);
+void Console_Menu_Config_SetEncoderCounterScalerUp( void);
+void Console_Menu_Config_SetRotorMinCounterDown( void);
+void Console_Menu_Config_SetRotorMinCounterUp( void);
+void Console_Menu_Config_SetRotorMaxCounterDown( void);
+void Console_Menu_Config_SetRotorMaxCounterUp( void);
 void Console_Menu_GotoMain( void);
 
 static Commandline::tCmdlineMenu cmdlineMenuConfig[] =
@@ -26,6 +33,13 @@ static Commandline::tCmdlineMenu cmdlineMenuConfig[] =
 	{"configWrite",					Console_Menu_ConfigWrite},
 	{"configShow",					Console_Menu_ConfigShow},
 	{"help",						Console_Menu_Config_Help},
+	{"setUseDhcp",					Console_Menu_Config_UseDhcp},
+	{"setEncoderCounterScalerDown",	Console_Menu_Config_SetEncoderCounterScalerDown},
+	{"setEncoderCounterScalerUp",	Console_Menu_Config_SetEncoderCounterScalerUp},
+	{"setRotorMinCounterDown",		Console_Menu_Config_SetRotorMinCounterDown},
+	{"setRotorMinCounterUp",		Console_Menu_Config_SetRotorMinCounterUp},
+	{"setRotorMaxCounterDown",		Console_Menu_Config_SetRotorMaxCounterDown},
+	{"setRotorMaxCounterUp",		Console_Menu_Config_SetRotorMaxCounterUp},
 	{"exit",						Console_Menu_GotoMain},
 };
 
@@ -59,6 +73,18 @@ void Console_Menu_ConfigShow( void)
 	clientTelnet.print( " / ");
 	clientTelnet.print( g_eepromStore.EncoderCounterScaler[R_DOWN]);
 	clientTelnet.println( "");
+
+	clientTelnet.print( "\tRotor Min Counter Up / Down:      ");
+	clientTelnet.print( g_eepromStore.RotorMinCounterInDegree[R_UP]);
+	clientTelnet.print( " / ");
+	clientTelnet.print( g_eepromStore.RotorMinCounterInDegree[R_DOWN]);
+	clientTelnet.println( " degree");
+
+	clientTelnet.print( "\tRotor Max Counter Up / Down:      ");
+	clientTelnet.print( g_eepromStore.RotorMaxCounterInDegree[R_UP]);
+	clientTelnet.print( " / ");
+	clientTelnet.print( g_eepromStore.RotorMaxCounterInDegree[R_DOWN]);
+	clientTelnet.println( " degree");
 }
 
 void Console_Menu_Config_Help( void)
@@ -70,8 +96,50 @@ void Console_Menu_Config_Help( void)
 		"*   configWrite                - Write Config toEEprom         *\r\n"
 		"*   configShow                 - Show Configuration            *\r\n"
 		"*   help                       - displays available commands   *\r\n"
+		"*   setUseDhcp <s>             - Set DHCP on start 0=off, 1=on *\r\n"
+		"*   setEncoderCounterScalerDown <v> - Set scaler in Down       *\r\n"
+		"*   setEncoderCounterScalerUp <v>   - Set scaler in Up         *\r\n"
+		"*   setRotorMinCounterDown <v> - Set Rotor min counter [degree]*\r\n"
+		"*   setRotorMinCounterUp <v>   - Set Rotor min counter [degree]*\r\n"
+		"*   setRotorMaxCounterDown <v> - Set Rotor max counter [degree]*\r\n"
+		"*   setRotorMaxCounterUp <v>   - Set Rotor max counter [degree]*\r\n"
 		"*   exit                       - Got Main Menu                 *\r\n"
 		"****************************************************************\r\n");
+}
+
+void Console_Menu_Config_UseDhcp( void)
+{
+	g_eepromStore.UseDhcp = cmd.cmdlineGetArgDouble(1);
+}
+
+void Console_Menu_Config_SetEncoderCounterScalerDown( void)
+{
+	g_eepromStore.EncoderCounterScaler[R_DOWN] = cmd.cmdlineGetArgDouble(1);
+}
+
+void Console_Menu_Config_SetEncoderCounterScalerUp( void)
+{
+	g_eepromStore.EncoderCounterScaler[R_UP] = cmd.cmdlineGetArgDouble(1);
+}
+
+void Console_Menu_Config_SetRotorMinCounterDown( void)
+{
+	g_eepromStore.RotorMinCounterInDegree[R_DOWN] = cmd.cmdlineGetArgDouble(1);
+}
+
+void Console_Menu_Config_SetRotorMinCounterUp( void)
+{
+	g_eepromStore.RotorMinCounterInDegree[R_UP] = cmd.cmdlineGetArgDouble(1);
+}
+
+void Console_Menu_Config_SetRotorMaxCounterDown( void)
+{
+	g_eepromStore.RotorMaxCounterInDegree[R_DOWN] = cmd.cmdlineGetArgDouble(1);
+}
+
+void Console_Menu_Config_SetRotorMaxCounterUp( void)
+{
+	g_eepromStore.RotorMaxCounterInDegree[R_UP] = cmd.cmdlineGetArgDouble(1);
 }
 
 void Console_Menu_GotoMain( void)
